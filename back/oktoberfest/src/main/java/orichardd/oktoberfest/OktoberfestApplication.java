cf1e7e14b3bbd83a8123df539847354e09d680a6
orichardd <richdd.dev@gmail.com>
2026-07-06 22:08:33 -0300

    V0.0 - Oktoberfest

---
+++ back/oktoberfest/src/main/java/orichardd/oktoberfest/OktoberfestApplication.java
@@ -0,0 +1,13 @@
+package orichardd.oktoberfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktoberfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktoberfestApplication.class, args);
+	}
+
+}
---
+++ back/oktoberfest/src/test/java/orichardd/oktoberfest/OktoberfestApplicationTests.java
@@ -0,0 +1,13 @@
+package orichardd.oktoberfest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktoberfestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
